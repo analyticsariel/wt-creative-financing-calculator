85bb54f62c633c933cc4f7c23e053021ad7c1765
Ariel <61569815+analyticsariel@users.noreply.github.com>
2024-07-12 14:13:16 -0400

    Add files via upload

--- pages/2_📈_Best_Terms.py
+++ pages/2_📈_Best_Terms.py
@@ -92,9 +92,49 @@ def optimize_terms(
 #                      APP                         #
 ####################################################
 st.title('Best Terms 📈')
-st.subheader('Enter details to calculate best offer terms')
-st.markdown('Want to see the properties 🏡 behind the numbers? Check out 👉 [Coffee Clozers](https://bit.ly/3EJRAa5) 🏡, we help real estate investors find cash flowing deals🤑 in up-and-coming areas within minutes ⏱️ to support their journey of financial freedom.')
+st.subheader('Enter deal details to INSTANTLY calculate best offer terms')
+st.markdown('Want to use the AI model? Check out 👉 [Coffee Clozers](https://bit.ly/3EJRAa5) 🏡, we help real estate investors find cash flowing deals🤑 in up-and-coming areas within minutes ⏱️ to support their journey of financial freedom.')
 
+import hmac
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
 
 
 ####################################################
